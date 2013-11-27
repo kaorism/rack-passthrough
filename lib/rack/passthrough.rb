5209f8561b5a184f9e5f85f3e582f2fce2895242
Jo <jo@smartsoftasia.com>
2013-11-27 16:03:49 +0700

    fix headers response

--- lib/rack/passthrough.rb
+++ lib/rack/passthrough.rb
@@ -10,19 +10,36 @@ module Rack
         @endpoint = endpoint
       end
 
+      def to_normal_string(data)
+        if data.class == Array
+          data = data.join
+        else
+          data
+        end
+
+        data
+      end
+
       def call(env)
         request = Rack::Request.new(env)
         full_path = URI::join( @endpoint, request.path )
         # Set Query String
         full_path.query = request.query_string
         data = request.body.read
-        body_data = JSON.parse( data ) unless data.blank?
+        # body_data = JSON.parse( data ) unless data.blank?
+        # body_data = Rack::Utils.parse_query( data ) unless data.blank?
         conn = HTTParty.method( request.request_method.downcase.to_sym )
 
         # Request to endpoint
-        response = conn.call( full_path.to_s, { body: body_data } )
+        response = conn.call( full_path.to_s, { body: env["action_dispatch.request.request_parameters"] } )
+        headers = {}
 
-        [ response.code , response.headers , [response.body] ]
+        response.headers.each do |key,value|
+          headers.merge!({ key => to_normal_string(value) })
+
+        end
+
+        [ response.code , headers , [response.body] ]
       end
     end
     
